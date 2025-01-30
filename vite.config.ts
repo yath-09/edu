e7cea77432ee8a82dc5ac99d8ebd38cb1d48cbda
Shubham Saurav <shubhamsr@Shubhams-MacBook-Air.local>
2025-01-29 20:52:28 -0500

    solved reloading issue

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
-})
+  server: {
+    port: 3000,
+    open: true
+  }
+});
